5264aae8e5664b6f7d6ba780eb0b2d8cd66559b5
João Ferreira dos Santos Neto <ifsbariela@gmail.com>
2022-03-31 14:39:56 -0300

    wip

--- src/main/java/com/jneto/crudspring/CrudSpringApplication.java
+++ src/main/java/com/jneto/crudspring/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.jneto.crudspring;
 
+import com.jneto.crudspring.model.Curso;
+import com.jneto.crudspring.repository.CursoRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CursoRepository cursoRepository) {
+		return args -> {
+			cursoRepository.deleteAll();
+
+			Curso c = new Curso();
+			c.setNome("Angular com Spring");
+			c.setCategoria("front-end");
+
+			cursoRepository.save(c);
+		};
 	}
 
 }
